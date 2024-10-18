03e7b782700f4471e966a00cf899e4f2912a9918
Cade Larrabee <larrabeecade@gmail.com>
2024-10-18 09:13:51 -0400

    Changing to node router to express route

--- app.js
+++ app.js
@@ -1,61 +1,46 @@
-var http = require("http");
 var fs = require("fs");
 var path = require("path");
+const express = require("express");
+const app = express();
 
-http
-  .createServer(function (req, res) {
-    if (req.url === "/index" || req.url === "/") {
-      fs.readFile(
-        path.join(__dirname, "html", "index.html"),
-        function (err, data) {
-          if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("500 Internal Server Error");
-          } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-          }
-        }
-      );
-    } else if (req.url === "/about") {
-      fs.readFile(
-        path.join(_dirname, "html", "about.html"),
-        function (err, data) {
-          if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("500 Internal Server Error");
-          } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-          }
-        }
-      );
-    } else if (req.url === "/contact") {
-      fs.readFile(
-        path.join(__dirname, "html", "contact-me.html"),
-        function (err, data) {
-          if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("500 Internal Server Error");
-          } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-          }
-        }
-      );
+app.get("/", (req, res) => {
+  console.log(path.join(__dirname, "html", "index.html"));
+  fs.readFile(path.join(__dirname, "html", "index.html"), function (err, data) {
+    if (err) {
+      res.status(500).send("Error loading Home Page");
     } else {
-      fs.readFile(
-        path.join(__dirname, "html", "404.html"),
-        function (err, data) {
-          if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("500 Internal Server Error");
-          } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-          }
-        }
-      );
+      res.set("Content-Type", "text/html");
+      res.send(data);
     }
-  })
-  .listen(8080);
+  });
+});
+
+app.get("/about", (req, res) => {
+  fs.readFile(path.join(__dirname, "html", "about.html"), function (err, data) {
+    if (err) {
+      res.status(500).send("Error loading About Page");
+    } else {
+      res.set("Content-Type", "text/html");
+      res.send(data);
+    }
+  });
+});
+
+app.get("/contact", (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "html", "contact.html"),
+    function (err, data) {
+      if (err) {
+        res.status(500).send("Error loading Contact Page");
+      } else {
+        res.set("Content-Type", "text/html");
+        res.send(data);
+      }
+    }
+  );
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
